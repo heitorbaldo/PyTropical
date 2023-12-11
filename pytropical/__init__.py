5253c273a7f2c027f1b0ee5f7726b9826a871341
Heitor Baldo <heitorb@protonmail.com>
2023-12-10 23:46:10 -0300

    Add files via upload

---
+++ pytropical/__init__.py
@@ -0,0 +1,11 @@
+"""
+Pytropical
+=========
+
+PyTropical is a Python package for tropical mathematics.
+
+See http://www.heitorbaldo.com/pytropical for complete documentation.
+"""
+
+#Current version:
+__version__ = "0.0.1"
